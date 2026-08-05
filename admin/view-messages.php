e1ca9e5224a738640daf4f22a5653c2645ed506f
MJ Verra <YOUR_GITHUB_EMAIL@example.com>
2026-08-05 18:24:52 +0800

    k

--- admin/view-messages.php
+++ admin/view-messages.php
@@ -27,7 +27,8 @@ if (!$result) {
 
     <title>Customer Messages | Hope Ways Realty</title>
 
-    <link rel="stylesheet" href="css/messages.css">
+    <link rel="stylesheet" href="../css/messages.css">
+
 
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">

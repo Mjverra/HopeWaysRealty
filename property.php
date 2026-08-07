a5c3c52d1eccf774eff195d156391a087ae11360
MJ Verra <marcjundelverra@gmail.com>
2026-08-07 13:32:36 +0800

    test

--- property.php
+++ property.php
@@ -185,7 +185,11 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
     <span class="close-lightbox">&times;</span>
 
+    <span class="prev-image">&#10094;</span>
+
     <img id="lightbox-image" src="" alt="">
+
+    <span class="next-image">&#10095;</span>
 
 </div>
 
@@ -212,6 +216,96 @@ galleryImages.forEach(image => {
 
 });
 
+<script>
+
+const galleryImages = document.querySelectorAll(".gallery-item img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImage = document.getElementById("lightbox-image");
+
+const closeBtn = document.querySelector(".close-lightbox");
+
+const prevBtn = document.querySelector(".prev-image");
+
+const nextBtn = document.querySelector(".next-image");
+
+let currentIndex = 0;
+
+function showImage(index){
+
+    lightbox.style.display = "flex";
+
+    lightboxImage.src = galleryImages[index].src;
+
+}
+
+galleryImages.forEach((image,index)=>{
+
+    image.addEventListener("click",()=>{
+
+        currentIndex=index;
+
+        showImage(currentIndex);
+
+    });
+
+});
+
+nextBtn.onclick=function(){
+
+    currentIndex++;
+
+    if(currentIndex>=galleryImages.length){
+
+        currentIndex=0;
+
+    }
+
+    showImage(currentIndex);
+
+};
+
+prevBtn.onclick=function(){
+
+    currentIndex--;
+
+    if(currentIndex<0){
+
+        currentIndex=galleryImages.length-1;
+
+    }
+
+    showImage(currentIndex);
+
+};
+
+document.addEventListener("keydown",function(e){
+
+    if(lightbox.style.display==="flex"){
+
+        if(e.key==="ArrowRight"){
+
+            nextBtn.click();
+
+        }
+
+        if(e.key==="ArrowLeft"){
+
+            prevBtn.click();
+
+        }
+
+        if(e.key==="Escape"){
+
+            lightbox.style.display="none";
+
+        }
+
+    }
+
+});
+
 closeBtn.onclick=function(){
 
     lightbox.style.display="none";
@@ -227,6 +321,8 @@ lightbox.onclick=function(e){
     }
 
 };
+
+</script>
 
 </script>
 

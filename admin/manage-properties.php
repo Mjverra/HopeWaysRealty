b690b38aa70367863cd5ba00f97a958c0bd4ad7d
MJ Verra <marcjundelverra@gmail.com>
2026-08-08 17:47:48 +0800

    jad

--- admin/manage-properties.php
+++ admin/manage-properties.php
@@ -155,7 +155,7 @@ $result = $stmt->get_result();
 
 <title>Property Management</title>
 
-<link rel="stylesheet" href="css/messages.css">
+
 
 <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">

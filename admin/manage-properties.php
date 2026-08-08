e3ae4b003ec8a3a211dec7c4a66c0095101a5dd5
MJ Verra <marcjundelverra@gmail.com>
2026-08-08 18:06:54 +0800

    adasd

--- admin/manage-properties.php
+++ admin/manage-properties.php
@@ -491,12 +491,44 @@ if($result->num_rows > 0){
 </div>
 
 <?php
-
 }
-
 ?>
 
-    </div>
+
+
+<?php if ($totalPages > 1): ?>
+
+<div class="pagination">
+
+    <?php if ($page > 1): ?>
+        <a href="?page=<?= $page-1 ?>&search=<?= urlencode($search) ?>&status=<?= urlencode($status) ?>&type=<?= urlencode($type) ?>">
+            &laquo; Previous
+        </a>
+    <?php endif; ?>
+
+    <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+
+        <a
+            href="?page=<?= $i ?>&search=<?= urlencode($search) ?>&status=<?= urlencode($status) ?>&type=<?= urlencode($type) ?>"
+            class="<?= ($i == $page) ? 'active' : '' ?>">
+
+            <?= $i ?>
+
+        </a>
+
+    <?php endfor; ?>
+
+    <?php if ($page < $totalPages): ?>
+
+        <a href="?page=<?= $page+1 ?>&search=<?= urlencode($search) ?>&status=<?= urlencode($status) ?>&type=<?= urlencode($type) ?>">
+            Next &raquo;
+        </a>
+
+    <?php endif; ?>
+
+</div>
+
+<?php endif; ?>
 
 </section>
 

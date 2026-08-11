2df46914cea6cd4e3a6592647475d8e6b313df66
MJ Verra <YOUR_GITHUB_EMAIL@example.com>
2026-08-11 18:58:51 +0800

    building

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -65,6 +65,7 @@ $totalAdmins = $conn->query("
 
     <link rel="stylesheet" href="../css/admin-header.css">
     <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/messages.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
--- admin/manage-admins.php
+++ admin/manage-admins.php
@@ -31,6 +31,7 @@ $result = $conn->query("
 
     <link rel="stylesheet" href="../css/admin-header.css">
     <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/messages.css">
 
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">

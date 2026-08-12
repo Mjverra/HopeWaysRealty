c6c045578fd24476eb7330391c07c544b1233b5b
MJ Verra <YOUR_GITHUB_EMAIL@example.com>
2026-08-12 11:39:41 +0800

    a

--- admin/edit-property.php
+++ admin/edit-property.php
@@ -82,7 +82,7 @@ $sql->close();
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Edit Property | HopeWays Realty</title>
+    <title>Edit Property || HopeWays Realty</title>
 
     <link rel="stylesheet" href="../css/messages.css">
     <link rel="stylesheet" href="../css/admin.css">
